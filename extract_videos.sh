b4e74e34a1741e51150dd7e44ea991dda109e515
Corentin Barreau <contact@corentinb.me>
2019-11-29 22:40:53 +0100

    Add: --force-ipv4 on youtube-dl

--- extract_videos.sh
+++ extract_videos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-youtube-dl -j --flat-playlist $1 | jq '.id' |  cut -d '"' -f 2 | ts 'https://youtube.com/watch?v=' | tr -d ' '
+youtube-dl --force-ipv4 -j --flat-playlist $1 | jq '.id' |  cut -d '"' -f 2 | ts 'https://youtube.com/watch?v=' | tr -d ' '
